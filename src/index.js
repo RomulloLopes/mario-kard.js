420647d003493944f714073459a626dd4955bc61
RomulloLopes <romullomattos77@gmail.com>
2025-03-08 15:40:55 -0300

    correção de concateção

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const player1 = {
-    NOME : "Mario",
-    VELOCIDADE : 4,
-    MANOBRABILIDADE : 3,
-    PODER : 3,
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
     PONTOS: 0,
 };
 
 const player2 = {
-    NOME : "Luigi",
-    VELOCIDADE : 3,
-    MANOBRABILIDADE : 4,
-    PODER : 4,
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
     PONTOS: 0,
 };
 
@@ -41,8 +41,7 @@ async function espaco() {
 
 async function logRollResult(characterName, block, diceResult, attribute) {
     console.log(
-        `${characterName}🎲 rolou um dado de ${block} ${diceResult} + ${attribute} = ${
-            diceResult + attribute
+        `${characterName}🎲 rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute
         }`);
 }
 
@@ -127,7 +126,7 @@ async function playRaceEngine(character1, character2) {
                 console.log("Empate! Ninguém perdeu ponto");
             }
             await espaco();
-        
+
             //verifica quem, entre os dois, tem o maior poder
             if (powerResult1 > powerResult2) {
                 console.log(`${character1.NOME} venceu o confronto!`);
@@ -146,12 +145,12 @@ async function declareWinner(character1, character2) {
     console.log("\nResultado final:");
     console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`);
     console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
-  
+
     if (character1.PONTOS > character2.PONTOS) {
-      console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`);
+        console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`);
 
     } else if (character2.PONTOS > character1.PONTOS) {
-      console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`);
+        console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`);
 
     } else console.log("A corrida terminou em empate");
 }
